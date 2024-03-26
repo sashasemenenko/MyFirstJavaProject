d8fa421a174344fce121d56451f6058b40f1ed55
Oleksandr Semenenko <115368382+sashasemenenko@users.noreply.github.com>
2024-03-26 11:50:45 +0100

    primitive data types

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int myFirstVariable;
+        myFirstVariable = 1;
+        System.out.println(myFirstVariable);
+
+        String name = "Oleksandr";
+        String textToPrint =
+                "Favourite number of " + name + " is: " + myFirstVariable;
+        System.out.println(textToPrint);
     }
 }
